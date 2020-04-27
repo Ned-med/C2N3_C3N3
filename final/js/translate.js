a1bf83808ac730297595f26d5d4b8904617220ef
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2020-04-27 23:44:32 +0000

    quiz styling and rtl - ltr directions

--- final/js/translate.js
+++ final/js/translate.js
@@ -1,7 +1,9 @@
 
 $(function () {
+    //Selectors
     var langBtn = document.getElementsByClassName('btn__lang');
     const RecDescription = document.getElementById('recommandation__desc');
+    const quizBody = document.querySelector('.quiz');
     var lang;
     //in case of jumping to another page we need to maintaine the language that the user chose
     if (localStorage.getItem("lang")) {
@@ -13,7 +15,7 @@ $(function () {
     };
 
 
-// Translate on click
+    // Translate on click
     $('.btn__lang').click(function () {
         lang = $(this).attr('id');
         // Translate
@@ -24,19 +26,19 @@ $(function () {
     });
 
 
-// The Actuale Translating Function
-function Translate(language) {
-   
-    // $('.lango').each(function (index, element) {
-    //     $(this).innerHTML = arrLang[language][$(this).attr('key')];
-    // });
-    let lango = document.getElementsByClassName('lango');
-    Array.prototype.forEach.call(lango, el => {
-        el.innerHTML = arrLang[language][el.getAttribute("key")];
-    });
-}
+    // The Actuale Translating Function
+    function Translate(language) {
 
-// testing what is the current language and changing it in the dom accordingly
+        // $('.lango').each(function (index, element) {
+        //     $(this).innerHTML = arrLang[language][$(this).attr('key')];
+        // });
+        let lango = document.getElementsByClassName('lango');
+        Array.prototype.forEach.call(lango, el => {
+            el.innerHTML = arrLang[language][el.getAttribute("key")];
+        });
+    }
+
+    // testing what is the current language and changing it in the dom accordingly
     function testLang() {
         if (lang === 'ar') {
             Array.prototype.forEach.call(langBtn, el => {
@@ -47,9 +49,13 @@ function Translate(language) {
             //Fliping Recommandation Section
             RecDescription.style.order = '2';
             RecDescription.style.direction = 'rtl';
+
+            //Fliping Quiz
+            quizBody ? quizBody.style.direction = 'rtl' : "";
+
             // document.getElementById('recommandation__description').style.direction = 'rtl';
             // Array.from(RecDescription).forEach(curr => curr.style.direction = 'rtl');
-            
+
 
         } else if (lang === 'fr') {
             Array.prototype.forEach.call(langBtn, el => {
@@ -60,6 +66,9 @@ function Translate(language) {
             //Fliping Recommandation Section
             RecDescription.style.order = '0';
             RecDescription.style.direction = 'ltr';
+            //Fliping Quiz
+            quizBody ? quizBody.style.direction = 'ltr' : "";
+            
 
             // document.getElementById('recommandation__description').style.direction = 'ltr';
             // Array.from(RecDescription).forEach(curr => curr.style.direction = 'ltr');
@@ -69,19 +78,19 @@ function Translate(language) {
         }
     }
 
-//Toggling nav style 
-const menuAdvice = document.querySelectorAll('.menu_advice');
-const menuHome = document.querySelectorAll('.menu_home');
-let page = window.location.pathname.split("/").pop();
-if(page === "index.html") {
-    Array.from(menuHome, el => el.classList.add('active'));
-    // menuHome.classList.add('active')
-    console.log('true home'); 
-} else if (page === "advice.html") {
-   Array.from(menuAdvice, el => el.classList.add('active'));
-//    menuAdvice.classList.add('active');
-   console.log('true advice'); 
-}
+    //Toggling nav style 
+    const menuAdvice = document.querySelectorAll('.menu_advice');
+    const menuHome = document.querySelectorAll('.menu_home');
+    let page = window.location.pathname.split("/").pop();
+    if (page === "index.html") {
+        Array.from(menuHome, el => el.classList.add('active'));
+        // menuHome.classList.add('active')
+        console.log('true home');
+    } else if (page === "advice.html") {
+        Array.from(menuAdvice, el => el.classList.add('active'));
+        //    menuAdvice.classList.add('active');
+        console.log('true advice');
+    }
 
 
 });
